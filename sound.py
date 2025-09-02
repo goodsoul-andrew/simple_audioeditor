7997f447403b71a0f95716cfed99561c101ca35f
Andrew Goodman <goodsoul.andrew@mail.ru>
2025-09-03 00:20:15 +0500

    сохраняет теги mp3

--- sound.py
+++ sound.py
@@ -3,22 +3,47 @@ import os
 import struct
 import uuid
 import wave
+import ffmpeg
+import mutagen
+from mutagen.id3 import ID3, TIT2, TPE1, TALB, TCON, TDRC, TRCK, APIC, PictureType
+from mutagen.mp3 import MP3
 
 from converters import mp3_to_wav, wav_to_mp3
 
 
 class Sound:
     def __init__(self, filename):
+        self.filename = filename
         if filename[-4:] == ".wav":
             Sound._from_wave(self, filename)
         elif filename[-4:] == ".mp3":
             Sound._from_mp3(self, filename)
+        else:
+            raise ValueError("Неподдерживаемый формат аудио")
 
     def _from_mp3(self, filename):
         tmp_name = str(uuid.uuid4()) + ".wav"
         mp3_to_wav(filename, tmp_name)
         Sound._from_wave(self, tmp_name)
         os.remove(tmp_name)
+        self._get_mp3_tags()
+
+    def _get_mp3_tags(self):
+        metadata = {}
+        audio = MP3(self.filename, ID3=ID3)
+        if audio.tags is None:
+            self.mp3_metadata = {}
+        # print(audio.tags)
+        if 'TIT2' in audio.tags: metadata['title'] = audio.tags['TIT2'].text[0] if audio.tags['TIT2'].text else None
+        if 'TPE1' in audio.tags: metadata['artist'] = audio.tags['TPE1'].text[0] if audio.tags['TPE1'].text else None
+        if 'TALB' in audio.tags: metadata['album'] = audio.tags['TALB'].text[0] if audio.tags['TALB'].text else None
+        if 'TCON' in audio.tags: metadata['genre'] = audio.tags['TCON'].text[0] if audio.tags['TCON'].text else None
+        if 'TRCK' in audio.tags: metadata['track_number'] = audio.tags['TRCK'].text[0] if audio.tags['TRCK'].text else None
+        if 'TDRC' in audio.tags: metadata['year'] = audio.tags['TDRC'].text[0] if audio.tags['TDRC'].text else None  # Год
+        if 'APIC:cover' in audio.tags:
+            metadata['cover_mime'] = audio.tags['APIC:cover'].mime
+            metadata['cover_data'] = audio.tags['APIC:cover'].data
+        self.mp3_metadata = metadata
 
     def _from_wave(self, filename: str):
         with wave.open(filename, "rb") as file:
@@ -97,3 +122,42 @@ class Sound:
         self.save_to_wave(tmp_name)
         wav_to_mp3(tmp_name, filename)
         os.remove(tmp_name)
+        audio = MP3(filename)
+        for tag_name, tag_value in self.mp3_metadata.items():
+            if tag_value is None:
+                continue
+            if tag_name == 'title':
+                audio.tags.add(TIT2(encoding=3, text=tag_value))
+            elif tag_name == 'artist':
+                audio.tags.add(TPE1(encoding=3, text=tag_value))
+            elif tag_name == 'album':
+                audio.tags.add(TALB(encoding=3, text=tag_value))
+            elif tag_name == 'genre':
+                audio.tags.add(TCON(encoding=3, text=tag_value))
+            elif tag_name == 'track_number':
+                audio.tags.add(TRCK(encoding=3, text=str(tag_value)))
+            elif tag_name == 'year':
+                audio.tags.add(TDRC(encoding=3, text=str(tag_value)))
+            elif tag_name == 'cover_path' and tag_value and os.path.exists(tag_value):
+                with open(tag_value, 'rb') as f:
+                    cover_data = f.read()
+                if tag_value.lower().endswith('.png'):
+                    mime_type = 'image/png'
+                elif tag_value.lower().endswith('.gif'):
+                    mime_type = 'image/gif'
+                audio.tags.add(APIC(
+                    encoding=3,  # UTF-8
+                    mime=mime_type,
+                    type=3,  # Front Cover
+                    desc='Front Cover',
+                    data=cover_data
+                ))
+            elif tag_name == 'cover_data':
+                audio.tags.add(APIC(
+                    encoding=3,  # UTF-8
+                    mime=self.mp3_metadata['cover_mime'],
+                    type=3,  # Front Cover
+                    desc='Front Cover',
+                    data=self.mp3_metadata['cover_data']
+                ))
+        audio.save()
